f8278a365a4c1d48a7e1e5381e6a15d21bffc6fb
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-09-25 21:32:44 +0900

    complete url_qr

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -7,18 +7,39 @@ class TopController < ApplicationController
   end
 
   def show
-    @qr_id = Qr.find_by(id: params[:id])
+    @qr = Qr.find_by(id: params[:id])
 
-    content = "#{@qr_id.url}"
-    size    = 3         
-    level   = :h            
-    @qr_code = RQRCode::QRCode.new(content, size: size, level: level).as_svg.html_safe
+    # content = "#{@qr.url}"
+    # size    = 3      
+    # level   = :h            
+    # @qr_code = RQRCode::QRCode.new(content, size: size, level: level).as_svg.html_safe
+
+
+    content = "#{@qr.url}"
+    size    = 10      
+    level   = :h      
+
+    @qr_code = RQRCode::QRCode.new(content, size: size, level: level)
+    png = @qr_code.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 300
+    )
+    File.binwrite("./public/qr_images/qrcode.png", png.to_s)
+    
   end
 
   def create
-    @qr = Qr.new(url: params[:url])
-    if @qr.save
-      redirect_to("/top/#{@qr.id}")
+    @qr_url = Qr.new(url: params[:url])
+    if @qr_url.save
+      redirect_to("/top/#{@qr_url.id}")
     end
   end
 end
