81db1bc94d224aa6b9c5087a91666cd6454d9604
ryu1221 <ryuchan.pool12210517@gmail.com>
2020-10-06 14:16:02 +0900

    database

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -7,6 +7,18 @@ class TopController < ApplicationController
     @qr_url = Qr.new(url: params[:url])
   end
 
+  def create
+    @qr_url = Qr.new(url: params[:url])
+    
+    if @qr_url.save
+      flash[:notice] = "QRコードが作成されました"
+      redirect_to("/top/#{@qr_url.id}")
+    else
+      flash[:notice] = "QRコードの作成に失敗しました"
+      render('index')
+    end
+  end
+
   def show
     @qr = Qr.find_by(id: params[:id])
 
@@ -29,20 +41,88 @@ class TopController < ApplicationController
     )
 
     File.binwrite("./public/qr_images/qrcode.png", png.to_s)
-    
   end
 
 
-  def create
-    @qr_url = Qr.new(url: params[:url])
+
+
+  # def file
+  # end
+
+  # def file_create
+  #   @qr_url = Qr.new(url: params[:url])
     
-    if @qr_url.save
-      flash[:notice] = "QRコードが作成されました"
-      redirect_to("/top/#{@qr_url.id}")
-    else
-      flash[:notice] = "QRコードの作成に失敗しました"
-      render('index')
-    end
-  end
+  #   if @qr_url.save
+  #     flash[:notice] = "QRコードが作成されました"
+  #     redirect_to("/top/#{@qr_url.id}/file_show")
+  #   else
+  #     flash[:notice] = "QRコードの作成に失敗しました"
+  #     render('file')
+  #   end
+  # end
+
+  # def file_show
+  #   @qr = Qr.find_by(id: params[:id])
+
+  #   content = "#{@qr.url}"
+  #   size    = 30     
+  #   level   = :h      
+
+  #   @qr_code = RQRCode::QRCode.new(content, size: size, level: level)
+  #   png = @qr_code.as_png(
+  #     bit_depth: 1,
+  #     border_modules: 4,
+  #     color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+  #     color: 'black',
+  #     file: nil,
+  #     fill: 'white',
+  #     module_px_size: 6,
+  #     resize_exactly_to: false,
+  #     resize_gte_to: false,
+  #     size: 300
+  #   )
+
+  #   File.binwrite("./public/qr_images/qrcode.png", png.to_s)
+  # end
+
+  # def wifi
+  # end
+
+  # def wifi_create
+  #   @qr_url = Qr.new(url: params[:url])
+    
+  #   if @qr_url.save
+  #     flash[:notice] = "QRコードが作成されました"
+  #     redirect_to("/top/#{@qr_url.id}")
+  #   else
+  #     flash[:notice] = "QRコードの作成に失敗しました"
+  #     render('index')
+  #   end
+  # end
+
+  # def wifi_show
+  #   @qr = Qr.find_by(id: params[:id])
+
+  #   content = "#{@qr.url}"
+  #   size    = 10      
+  #   level   = :h      
+
+  #   @qr_code = RQRCode::QRCode.new(content, size: size, level: level)
+  #   png = @qr_code.as_png(
+  #     bit_depth: 1,
+  #     border_modules: 4,
+  #     color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+  #     color: 'black',
+  #     file: nil,
+  #     fill: 'white',
+  #     module_px_size: 6,
+  #     resize_exactly_to: false,
+  #     resize_gte_to: false,
+  #     size: 300
+  #   )
+
+  #   File.binwrite("./public/qr_images/qrcode.png", png.to_s)
+  # end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
 
-  resources :top, only:['index', 'show', 'create']
+  resources :top, only:['index', 'show', 'create'] do
+    # collection do
+    #   get 'file'
+    #   post 'file' => 'top#file_create'
+    #   get 'wifi'
+    #   post 'wifi' => 'top#wifi_create'
+    # end
+    # member do
+    #   get 'file_show'
+    #   get 'wifi_show'
+    # end
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
